e206c382cd316c1b7422daeddaf17454e15955b4
alex <alexey.0211@gmail.com>
2017-11-11 13:36:19 +0300

    Initial commit

---
+++ blog.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+
+<head>
+<title>Новости</title>
+
+
+  <!-- IE Fix for HTML5 Tags -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+    <? include 'head.php';?>
+</head>
+
+<body>
+
+<!-- ######################## Main Menu ######################## -->
+
+
+        
+<!-- ######################## Header (featured posts) ######################## -->
+
+<a href="index.html">
+    <div class="home"><i class="fa fa-home"></i></div>
+</a>
+
+<header>
+
+   
+
+        <div class="row">
+        
+        <h1>Популярные новости</h1>
+        
+<article class="four columns featured_post">
+        
+             <h3>Каникулярный клуб дневного пребывания! </h3> 
+              <img src="assets/img/news/1.png" alt="desc" style="width:100%" />
+              <p> На время летних каникул с 29 мая по 31 августа (кроме выходных) приглашаем детей 6-11 лет в 
+Каникулярный клуб дневного пребывания! 
+Время работы с 9:00 до 17:30! </p>
+              
+             
+         
+</article>
+
+         
+      
+<article class="four columns featured_post">
+        
+             <h3>Первая вечеринка PRIME Party ! </h3> 
+              <img src="assets/img/news/2.png" alt="desc" style="width:100%" />
+              <p> 8 июня состоялась первая летняя вечеринка PRIME. Изысканные закуски от ресторана @st_tropez_poolcafe и самая уютная компания.
+Фооточет уже на нашей странице Facebook.</p>
+         
+</article>
+          
+          
+
+<article class="four columns featured_post">
+        
+             <h3>9 апреля детские соревнования по плаванию. </h3> 
+              <img src="assets/img/news/3.png" alt="desc" style="width:100%" />
+              <p> Дорогие гости! 9 апреля состоятся детские соревнования по плаванию. Приглашаем к участию детей от 4 до 15 лет.  </p>
+         
+</article>
+
+
+        </div>
+        
+</header>
+      
+<!-- ######################## Section ######################## -->
+
+<section>
+
+  <div class="section_main">
+   
+      <div class="row">
+      
+          <section class="eight columns">
+          
+          <h2>Последние новости</h2>
+          
+          <article class="blog_post">
+          
+             <div class="three columns">
+             <a href="#" class="th"><img src="assets/img/news/4.png" alt="desc" /></a>
+             </div>
+             <div class="nine columns">
+              <a href="#"><h3>Ночь Фитнеса 2017</h3></a>
+              <p> 25 мая состоялось грандиозное событие Ночь Фитнеса 2017. Удивили гостей, разыграли 18 месяцев фитнеса и еще множество подарков от партнеров; а к разгару ночи все отправились на after Party в ресторан PARK Culture.</p>
+              </div>
+              
+          </article>
+          
+          <article class="blog_post">
+          
+             <div class="three columns">
+             <a href="#" class="th"><img src="assets/img/news/5.png" alt="desc" /></a>
+             </div>
+             <div class="nine columns">
+              <a href="#"><h3>Мастер-класс от Alessandro Oliveri</h3></a>
+              <p> 27 мая прошли невероятные мастер - классы от нашего большого друга и международного фитнес - презентера Alessandro Oliveri. Яркий фотоотчет доступен на нашей оффициальной странице FACEBOOK</p>
+              </div>
+              
+          </article>
+          
+          
+          <article class="blog_post">
+          
+             <div class="three columns">
+             <a href="#" class="th"><img src="assets/img/news/6.png" alt="desc" /></a>
+             </div>
+             <div class="nine columns">
+              <a href="#"><h3>SKILL MILL с Ариной Бессаловой!</h3></a>
+              <p> Каждый понедельник в 18:00 и среду в 20:00. Вас ждет невероятно азартная тренировка, направленная на развитие ловкости, координации, развития кардиореспираторной системы, с использованием разнообразного функционального оборудования. Предварительная запись обязательна.</p>
+              </div>
+              
+          </article>
+          
+          </section>
+          
+          <section class="four columns">
+          <h2></h2>
+             <div class="panel">
+                 <div class="col-lg-4 name">
+                     <img class="img-responsive" src="assets/img/pic.png">
+                     <br/>
+                     <br/>
+                     <a href="https://itunes.apple.com/ru/app/prime/id910475830?ls=1&mt=8" target="_blank"><img src="assets/img/as.png"></a>
+                     <a href="https://play.google.com/store/apps/details?id=me.fitcloud.app.primesport.android" target="_blank"><img src="assets/img/gp.png"></a>
+                 </div>
+
+
+
+               
+             </div>
+          </section>
+          
+      </div>
+      
+    </div>
+      
+</section>
+
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,19 @@
+<nav class="menu" id="theMenu">
+		<div class="menu-wrap">
+			<h1 class="logo"><a href="index.html#home">PRIME</a></h1>
+			<i class="fa fa-times menu-close"></i>
+			<a href="#home" class="smoothScroll">КЛУБНЫЕ КАРТЫ</a>
+			<a href="#about" class="smoothScroll">РАСПИСАНИЕ</a>
+			<a href="#portfolio" class="smoothScroll">НОВОСТИ</a>
+			<a href="#interer" class="smoothScroll">ИНТЕРЬЕР</a>
+			<a href="#services" class="smoothScroll">ВАКАНСИИ</a>
+			<a href="#contact" class="smoothScroll">КОНТАКТЫ</a>
+			<a href="#"><i class="fa fa-facebook"></i></a>
+			<a href="#"><i class="fa fa-twitter"></i></a>
+			<a href="#"><i class="fa fa-dribbble"></i></a>
+			<a href="#"><i class="fa fa-envelope"></i></a>
+		</div>
+		
+		<!-- Menu button -->
+		<div id="menuToggle"><i class="fa fa-bars"></i></div>
+	</nav>
